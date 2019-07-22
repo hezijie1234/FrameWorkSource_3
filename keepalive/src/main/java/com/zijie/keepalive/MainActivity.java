07f7c1ca865382c9ac5755a641754aa785ee56da
何子杰 <1710276127@qq.com>
2019-07-22 10:20:35 +0800

    进程拉活

--- keepalive/src/main/java/com/zijie/keepalive/MainActivity.java
+++ keepalive/src/main/java/com/zijie/keepalive/MainActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zijie.keepalive.account.AccountHelper;
+import com.zijie.keepalive.jobscheduler.MyJobService;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        KeepManager.getmInstance().registerKeepBroad(this);
-        startService(new Intent(this,ForegroundService.class));
+//        KeepManager.getmInstance().registerKeepBroad(this);
+//        startService(new Intent(this,ForegroundService.class));
+//        startService(new Intent(this,StickyService.class));
+//
+//        AccountHelper.addAccount(this);
+//        AccountHelper.autoSync();
+
+        //限制比较大，很多手机厂商做了限制无法使用
+        MyJobService.startJob(this);
     }
 
     @Override
